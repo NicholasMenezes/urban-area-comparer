7da25e2a25dc18b2bf93c224f0caacdd163ac245
nick menezes <nmenezes@kcftech.com>
2022-04-16 11:04:13 -0400

    Layout for urban area cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Container, Card } from "react-bootstrap";
+
 function App() {
-  return <div className="App">Blank App</div>;
+  return (
+    <div className="App">
+      <Container
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "20px",
+          maxWidth: "60rem",
+          marginTop: "2em"
+        }}
+      >
+        <Card style={{ width: "20rem", flexGrow: "1" }}>
+          <Card.Body>
+            <Card.Title>Urban Area 1</Card.Title>
+            <Card.Text>Urban Area Data Points</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "20rem", flexGrow: "1" }}>
+          <Card.Body>
+            <Card.Title>Urban Area 2</Card.Title>
+            <Card.Text>Urban Area Data Points</Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
