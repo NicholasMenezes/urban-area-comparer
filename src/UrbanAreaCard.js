ca3a3bd0d16f3d82fd21757aabdde39cae102cfb
nick menezes <nmenezes@kcftech.com>
2022-04-16 12:26:52 -0400

    Add Table to Display Data

--- src/UrbanAreaCard.js
+++ src/UrbanAreaCard.js
@@ -1,4 +1,4 @@
-import { Card, Form } from "react-bootstrap";
+import { Card, Form, Table } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import { GetUrbanAreaBySlug } from "./teleport-api";
 
@@ -22,6 +22,46 @@ function UrbanAreaCard(props) {
             <option value={u.href}>{u.name}</option>
           ))}
         </Form.Select>
+        {selectedUrbanArea ? (
+          <div>
+            <Table striped bordered hover style={{ marginTop: "1rem" }}>
+              <tbody>
+                <tr>
+                  <td>Full Name</td>
+                  <td>{selectedUrbanArea["full_name"]}</td>
+                </tr>
+                <tr>
+                  <td>Continent</td>
+                  <td>{selectedUrbanArea["continent"]}</td>
+                </tr>
+                <tr>
+                  <td>Mayor</td>
+                  <td>{selectedUrbanArea["mayor"]}</td>
+                </tr>
+                <tr>
+                  <td>Eastern Boundery</td>
+                  <td>{selectedUrbanArea["bounding_box"]["latlon"]["east"]}</td>
+                </tr>
+                <tr>
+                  <td>Western Boundery</td>
+                  <td>{selectedUrbanArea["bounding_box"]["latlon"]["west"]}</td>
+                </tr>
+                <tr>
+                  <td>Southern Boundery</td>
+                  <td>
+                    {selectedUrbanArea["bounding_box"]["latlon"]["south"]}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Northern Boundery</td>
+                  <td>
+                    {selectedUrbanArea["bounding_box"]["latlon"]["north"]}
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        ) : null}
       </Card.Body>
     </Card>
   );
