008765ea6a89ff0ed522a070542aba7c6ce8795b
nick menezes <nmenezes@kcftech.com>
2022-04-16 11:17:32 -0400

    Add helper method to get urban areas from API

---
+++ src/teleport-api.js
@@ -0,0 +1,8 @@
+export async function GetUrbanAreas() {
+  // TODO: Handle API errors
+  const apiResult = await fetch(
+    "https://api.teleport.org/api/urban_areas/"
+  ).then(r => r.json());
+
+  return [apiResult["_links"]["ua:item"]];
+}
